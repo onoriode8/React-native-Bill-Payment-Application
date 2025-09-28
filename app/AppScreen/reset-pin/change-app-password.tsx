6ae3441c2340dead60fed642808bfd907292ac0e
Onoriode <onoriodeumukoro8@gmail.com>
2025-09-28 07:33:39 +0100

    created component for changing app password

---
+++ app/AppScreen/reset-pin/change-app-password.tsx
@@ -0,0 +1,23 @@
+import { SafeAreaView, View } from "react-native";
+
+import { useChangeAppPassword } from '../../custom/reset-app-password'
+import ResetAppPasswordComponent from "../../util/reset-data/change-password"
+
+
+export default function ChangeAppPassword() {
+    const { setOldAppPasswordHandler, setNewAppPasswordHandler, loading,
+        setConfirmedAppPasswordHandler, resetAppPasswordHandler } = useChangeAppPassword();
+
+    return (
+        <SafeAreaView>
+            <View>
+                <ResetAppPasswordComponent resetAppPasswordHandler={resetAppPasswordHandler}
+                    oldAppPasswordVisible={true} loading={loading} 
+                    setOldAppPasswordHandler={setOldAppPasswordHandler}
+                    setNewAppPasswordHandler={setNewAppPasswordHandler}
+                    setConfirmedAppPasswordHandler={setConfirmedAppPasswordHandler}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
